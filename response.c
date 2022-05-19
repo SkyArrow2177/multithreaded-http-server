f3e0502756a5eec9a227269faa9f250cb4e96254
Ian Chen <ianchen7777@gmail.com>
2022-05-19 12:16:24 +1000

    cleanup: remove unnecessary #includes with iwyu

--- response.c
+++ response.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include "response.h"
--- server.c
+++ server.c
@@ -1,23 +1,10 @@
-#define _POSIX_C_SOURCE 200112L
-#include <assert.h>
 #include <dirent.h>
 #include <errno.h>
 #include <limits.h>
-#include <netdb.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/sendfile.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <unistd.h>
 
-#include "http.h"
-#include "response.h"
 #include "server_looper.h"
 
 // Features:
